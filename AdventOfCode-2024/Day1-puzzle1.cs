a890a70065eae7d5d57f7ff02e944c03a7414285
Grzegorz K <kubol89@gmail.com>
2024-12-04 06:08:05 +0100

    1. Created Advent of Code solution. 2. Created solution for Day1 first puzzle.

---
+++ AdventOfCode-2024/Day1-puzzle1.cs
@@ -0,0 +1,29 @@
+﻿#region "Input Declaration"
+
+var firstList = new List<int>()
+{
+    (18944),
+    (94847),
+};
+
+var secondList = new List<int>()
+{
+    (47230),
+    (63037),
+};
+
+#endregion
+
+firstList.Sort();
+secondList.Sort();
+
+var distanceCalculation = new List<int>();
+
+for(int i = 0; i < firstList.Count; ++i)
+{
+    distanceCalculation.Add(Math.Abs(firstList[i] - secondList[i]));
+}
+
+var sum = distanceCalculation.Sum();
+
+Console.WriteLine(sum);
